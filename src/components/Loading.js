329cd17d6360e6659f2f8b72767a224aa11565a0
Naim <istemi1016@gmail.com>
2024-07-07 14:33:40 +0300

    cfvsd

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import { Image } from 'react-native';
-
-function Loading({width}) {
-    return (    <Image source={require('../assets/loading.gif')} style={{   width: width||30,
-        height: width||30  ,
-        marginBottom: 8,}} /> );
-}
-
-export default Loading;
--- src/helpers/emailValidator.js
+++
@@ -1,8 +0,0 @@
-import LangApp from "../components/Language"
-
-export function emailValidator(email) {
-  const re = /\S+@\S+\.\S+/
-  if (!email) return LangApp("Emailcantbeempty")
-  if (!re.test(email)) return 'Ooops! We need a valid email address.'
-  return ''
-}
